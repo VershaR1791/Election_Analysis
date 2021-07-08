c5d5cf7eb9c89e6c2951b494f269271ab20b8b5b
VershaR1791 <r.versha91@gmail.com>
2021-07-08 00:04:47 -0400

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -18,6 +18,11 @@ candidate_options = []
 #Declaring a dictionary
 candidate_votes ={}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -48,14 +53,44 @@ with open(file_to_load) as election_data:
         candidate_votes[candidate_name] += 1
 
 #Print the total votes.
-print(total_votes)
+print(f"The total number of votes casted in Colorado were {total_votes}")
 
 #print the candidate list
-print(candidate_options)
+print(f"The candidates who contested the election were: {candidate_options}")
 
 #print the candidate vote dictionary
-print(candidate_votes)
+print(f"The votes received by each candidate were: {candidate_votes}")
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+#Iterate through the candidate list.
+for candidate_name in candidate_votes:
+
+    #Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+
+    #Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #Determine winning vote count and candidate
+    #Determine if the votes are greater than winning count
+    if(votes > winning_count) and (vote_percentage > winning_percentage):
+
+        #if condition is true
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+        #Print the candidate name and percentage of votes.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
     
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
 #1. The total number of votes cast
 #2. A complete list of candidates who received votes
 #3. The percentage of votes each candidate won
---
+++ Python_practice.py
@@ -0,0 +1,3 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voter in counties_dict.items():
+        print(f'{county} county has {voter} registered voters')
